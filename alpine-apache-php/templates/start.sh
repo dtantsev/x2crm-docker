b6ebdd11909d333b96a3cace4754b1fa945175ca
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-01-11 22:12:30 +0200

    add command to remove pid httpd before start httpd

--- alpine-apache-php/templates/start.sh
+++ alpine-apache-php/templates/start.sh
@@ -5,5 +5,5 @@ sed -i "s#/var/www/localhost/htdocs#/app/$WEBAPP_ROOT#" /etc/apache2/httpd.conf
 printf "\n<Directory \"/app/$WEBAPP_ROOT\">\n\tAllowOverride All\n</Directory>\n" >> /etc/apache2/httpd.conf
 chown -R apache:apache /app
 crond -l 2
-
+rm -f /run/apache2/httpd.pid
 httpd -DFOREGROUND
